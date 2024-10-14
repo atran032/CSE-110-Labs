0581e2c8c8c161b6395e0f426c386de284b783ca
atran032 <ant032@ucsd.edu>
2024-10-14 15:42:03 -0700

    testing progress

--- lab3/my-app/src/stickyNotes.test.tsx
+++ lab3/my-app/src/stickyNotes.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { StickyNotes } from "./stickyNotes";
+import exp from "constants";
 
 describe("Create StickyNote", () => {
   test("renders create note form", () => {
@@ -35,6 +36,23 @@ describe("Create StickyNote", () => {
 describe("required tests", () => {
     test("read", () => {
         render(<StickyNotes />);
+        const note1 = screen.getByText("test note 1 title");
+        const note2 = screen.getByText("test note 2 title");
+        const note3 = screen.getByText("test note 3 title");
+        const note4 = screen.getByText("test note 4 title");
+        const note5 = screen.getByText("test note 5 title");
+        const note6 = screen.getByText("test note 6 title");
+
+        expect(note1).toBeInTheDocument();
+        expect(note2).toBeInTheDocument();
+        expect(note3).toBeInTheDocument();
+        expect(note4).toBeInTheDocument();
+        expect(note5).toBeInTheDocument();
+        expect(note6).toBeInTheDocument();
+    });
+
+    test("update", () => {
+        render(<StickyNotes />);
         const createNoteTitleInput = screen.getByPlaceholderText("Note Title");
         const createNoteContentTextarea =
           screen.getByPlaceholderText("Note Content");
@@ -46,16 +64,43 @@ describe("required tests", () => {
         });
         fireEvent.click(createNoteButton);
 
-        
-    });
+        const newNoteTitle = screen.getByText("New Note");
+        const newNoteContent = screen.getByText("Note Content");
 
-    test("update", () => {
-        render(<StickyNotes />);
+        fireEvent.click(newNoteTitle);
+        fireEvent.change(newNoteTitle, { target: { value: "Hello" } });
+        fireEvent.click(newNoteContent)
+        fireEvent.change(newNoteContent, {
+          target: { value: "World" },
+        });
 
+        const Hello = screen.getByText("Hello");
+        const World = screen.getByText("World");
+
+        expect(Hello).toBeInTheDocument();
+        expect(World).toBeInTheDocument();
     });
 
     test("delete", () => {
         render(<StickyNotes />);
+        const createNoteTitleInput = screen.getByPlaceholderText("Note Title");
+        const createNoteContentTextarea =
+          screen.getByPlaceholderText("Note Content");
+        const createNoteButton = screen.getByText("Create Note");
+    
+        fireEvent.change(createNoteTitleInput, { target: { value: "New Note" } });
+        fireEvent.change(createNoteContentTextarea, {
+          target: { value: "Note Content" },
+        });
+        fireEvent.click(createNoteButton);
 
+        const newNoteTitle = screen.getByText("New Note");
+        const newNoteContent = screen.getByText("Note Content");
+
+        const deleteButton = screen.getByText("X");
+        fireEvent.click(deleteButton);
+
+        expect(newNoteTitle).not.toBeInTheDocument();
+        expect(newNoteTitle).not.toBeInTheDocument();
     });
 });
--- lab3/my-app/src/toDoList.test.tsx
+++ lab3/my-app/src/toDoList.test.tsx
@@ -4,11 +4,14 @@ import { ToDoList } from "./toDoList";
 describe ("required tests", () => {
     test("read", () => {
         render(<ToDoList />);
-        
+        const apples = screen.getByText("Apples")
+        const bananas = screen.getByText("Bananas")
+
+        expect(apples).toBeInTheDocument();
+        expect(bananas).toBeInTheDocument();
     });
 
     test("count", () => {
-        render(<ToDoList />);
-
+        
     });
 });
