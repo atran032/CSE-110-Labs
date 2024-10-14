7d3b3892245d76a354d2a294cbb9977643d4f5a0
atran032 <ant032@ucsd.edu>
2024-10-14 14:01:53 -0700

    part 1 done

--- lab3/my-app/src/navbar.tsx
+++ lab3/my-app/src/navbar.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import { themes } from "./themeContext"
 
 export const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link> <Link to="/todolist">To Do List</Link>
-      </nav>
-    </div>
+	<div>
+  	<nav style={{ display: "flex", justifyContent: "space-around", background: themes.light.background}}>
+      <Link to="/">Home</Link>
+      <Link to="/todolist/ABC">ABC To Do List</Link>
+      <Link to="/todolist/DEF">DEF To Do List</Link>
+  	</nav>
+	</div>
   );
 };
