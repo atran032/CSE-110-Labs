5e0bac0d73ab264db7ada6d2c751ec88ee88dde2
atran032 <ant032@ucsd.edu>
2024-10-14 00:45:20 -0700

    Re-add my-app as a normal folder

---
+++ lab2/my-app/src/constant.ts
@@ -0,0 +1,46 @@
+import { Label, Note } from "./types"; // Import the Label type from the appropriate module
+
+export const dummyNotesList = [
+    {
+        id: 1,
+        title: "test note 1 title",
+        content: "test note 1 content",
+        label: Label.other,
+        like: false,
+    },
+    {
+        id: 2,
+        title: "test note 2 title",
+        content: "test note 2 content",
+        label: Label.personal,
+        like: false,
+    },
+    {
+        id: 3,
+        title: "test note 3 title",
+        content: "test note 3 content",
+        label: Label.work,
+        like: false,
+    },
+    {
+        id: 4,
+        title: "test note 4 title",
+        content: "test note 4 content",
+        label: Label.study,
+        like: false,
+    },
+    {
+        id: 5,
+        title: "test note 5 title",
+        content: "test note 5 content",
+        label: Label.study,
+        like: false,
+    },
+    {
+        id: 6,
+        title: "test note 6 title",
+        content: "test note 6 content",
+        label: Label.personal,
+        like: false,
+    },
+ ]
